47fa7156ac8f05b1c18457b52315ed9c00bcefb5
2siuuuu <dltn09194141@gmail.com>
2024-05-24 17:36:38 +0900

    test check box

--- window.py
+++ window.py
@@ -1,10 +1,26 @@
 from tkinter import *
+from tkinter import messagebox
 
-window = Tk()
+mainscreen = Tk()
 
-window.title("윈도우 창 연습")
-window.geometry("400x100")
-window.resizable(width = FALSE, height = FALSE)
+#---------------------------------------------------------
 
-window.mainloop()
+def window_setup():
+    mainscreen.title("윈도우 창 연습")
+    mainscreen.geometry("800x400") 
+    mainscreen.resizable(width = FALSE, height = FALSE)
+
+def reveal_messagebox():
+    messagebox.showinfo("왜 눌러", "나가라")
+
+#---------------------------------------------------------
+
+TEST_Button = Button(mainscreen, text="TEST!", command= reveal_messagebox)
+TEST_Button.pack()
+
+window_setup()
+
+
+
+mainscreen.mainloop()
 #develop에 추가
